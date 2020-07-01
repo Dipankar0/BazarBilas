14f5e12fa46b68982377d0a47db59961654c51f3
Dipankar Bhadra <dipankarbhadra@Dipankars-MacBook-Pro.local>
2020-07-01 19:58:23 +0600

    added yml file

---
+++ client/build/precache-manifest.0224a09eb7a26774c1d64800e2e1e2db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6554ba43f223ef4c0a5e48e412d999a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eead0950fd730bf9f186",
+    "url": "/static/css/main.99e1e2e0.chunk.css"
+  },
+  {
+    "revision": "5f9d39501394959d1356",
+    "url": "/static/js/2.d64bc492.chunk.js"
+  },
+  {
+    "revision": "276ddf9842587b4f3d449e1253efa054",
+    "url": "/static/js/2.d64bc492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eead0950fd730bf9f186",
+    "url": "/static/js/main.88df0df2.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "39a96206701683202ff28c673641efd3",
+    "url": "/static/media/landingPromo.39a96206.jpg"
+  },
+  {
+    "revision": "3ab65dfc4e2cce05051e5ae4745e9884",
+    "url": "/static/media/logo.3ab65dfc.jpeg"
+  },
+  {
+    "revision": "a0b72be436c74614e42b678d48f17121",
+    "url": "/static/media/organic.a0b72be4.jpg"
+  },
+  {
+    "revision": "6f09789b3650152754b74990aec52be4",
+    "url": "/static/media/spinner.6f09789b.gif"
+  }
+]);
