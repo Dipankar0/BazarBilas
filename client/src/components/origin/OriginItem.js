4e4c5b31045fbf85f82de1fbf11a948d470feb06
Dipankar Bhadra <dipankarbhadra@Dipankars-MacBook-Pro.local>
2020-07-06 16:55:59 +0600

    lets try

--- client/src/components/origin/OriginItem.js
+++ client/src/components/origin/OriginItem.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Carousel from 'react-material-ui-carousel';
 import { Grid } from '@material-ui/core';
 
 const OriginItem = ({ origin: { name, files, _id } }) => {
